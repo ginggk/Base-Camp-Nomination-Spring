0533c55ffef02fc47db9710cece9bf0833ce2d2e
gingerkeys <gkeys@basecampcodingacademy.org>
2019-02-26 08:55:41 -0600

    first commit

---
+++ src/main/java/com/gingerraymatthew/basecampnomination/BasecampnominationApplication.java
@@ -0,0 +1,13 @@
+package com.gingerraymatthew.basecampnomination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasecampnominationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasecampnominationApplication.class, args);
+	}
+
+}
